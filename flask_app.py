589b6f456453c52425b2ac62e1865221dad16d54
PJryn <peregrinryan@gmail.com>
2022-11-24 18:26:07 +1100

    Added Json function

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,19 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 import config
-# create the extension
-db = SQLAlchemy()
+
+import numpy as np
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
 # create the app
 app = Flask(__name__)
+
+
 protocol = 'postgresql'
 username = config.Username
 password = config.Password
@@ -13,9 +22,14 @@ host = 'localhost'
 port = 5432
 database_name = 'olympics'
 # configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = f'{protocol}://{username}:{password}@{host}:{port}/{database_name}'
+engine = create_engine(f'{protocol}://{username}:{password}@{host}:{port}/{database_name}')
 # initialize the app with the extension
-db.init_app(app)
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Passenger = Base.classes.country
 
 # 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
