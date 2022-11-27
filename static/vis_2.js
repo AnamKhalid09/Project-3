612aeea212a30a0ca0e528b97072922fd54f4193
zitarashe <zeeokot@gmail.com>
2022-11-27 17:19:29 +1100

    Add chart title, axis titles and series labels

--- static/vis_2.js
+++ static/vis_2.js
@@ -3,25 +3,39 @@ url = 'http://127.0.0.1:5000/api';
 d3.json(url).then(function(data) {
     console.log(data);
 
-    displayGdpChart(data);
+    data.sort((a, b) => a.total_won < b.total_won ? 1 : -1);
+    displayGdpChart(data.slice(0,10));
   });
 
 function displayGdpChart(data) {
     console.log("display gdp chart");
     let barData = {
-        type: "line",
-        showlegend: false,
-        x: data.map(d=>d.country),
-        y: data.map(d=>parseInt(d.gdp))
-    };
-
-    let lineData = {
-        type: "line",
-        showlegend: false,
+        name: "Total Won",
+        type: "scatter",
         x: data.map(d=>d.country),
         y: data.map(d=>parseInt(d.total_won))
     };
 
-    Plotly.newPlot("plot", [barData, lineData]);
+    let lineData = {
+        name: "GDP",
+        type: "scatter",
+        yaxis: 'y2',
+        x: data.map(d=>d.country),
+        y: data.map(d=>parseInt(d.gdp))
+    };
+
+    let layout = {
+        title: 'Total Won vs GDP',
+        yaxis: {title: 'Total Won'},
+        yaxis2: {
+          title: 'GDP',
+          titlefont: {color: 'rgb(148, 103, 189)'},
+          tickfont: {color: 'rgb(148, 103, 189)'},
+          overlaying: 'y',
+          side: 'right'
+        }
+      };
+      
+    Plotly.newPlot("plot", [barData, lineData], layout);
 }
 
--- static/vis_3.js
+++ static/vis_3.js
@@ -10,18 +10,31 @@ d3.json(url).then(function(data) {
 function displayPopulationChart(data) {
     console.log("display population chart");
     let barData = {
+        name: "Total Won",
         type: "scatter",
-        showlegend: false,
+        x: data.map(d=>d.country),
+        y: data.map(d=>parseInt(d.total_won))
+    };
+    let lineData = {
+        name: "Population",
+        type: "scatter",
+        yaxis: 'y2',
         x: data.map(d=>d.country),
         y: data.map(d=>parseInt(d.population))
     };
 
-    let lineData = {
-        type: "scatter",
-        showlegend: false,
-        x: data.map(d=>d.country),
-        y: data.map(d=>parseInt(d.total_won))
-    };
 
-    Plotly.newPlot("plot", [barData, lineData]);
+    let layout = {
+        title: 'Total Won vs Population',
+        yaxis: {title: 'Total Won'},
+        yaxis2: {
+          title: 'Population',
+          titlefont: {color: 'rgb(148, 103, 189)'},
+          tickfont: {color: 'rgb(148, 103, 189)'},
+          overlaying: 'y',
+          side: 'right'
+        }
+      };
+      
+    Plotly.newPlot("plot", [barData, lineData], layout);
 }
